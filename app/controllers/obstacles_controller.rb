d5ba0cc953763537c82027db87a3b160cdc945e1
Rosibel Cisneros <kelsocisneros@gmail.com>
2017-07-26 09:20:44 -0700

    log in and log out works and obstacles are posting

--- app/controllers/obstacles_controller.rb
+++ app/controllers/obstacles_controller.rb
@@ -11,14 +11,12 @@ class ObstaclesController < ApplicationController
 
     def create
       @obstacle = Obstacle.new(params.require(:obstacle).permit(:obstacle_content))
-
-      @obstacle.person = current_peep
-
-      if @obstacle.save
-        redirect_to obstacles_path
-      else
-        render :new
-      end
+        @obstacle.person = current_peep 
+          if @obstacle.save
+            redirect_to obstacles_path
+          else
+            render :new
+          end
     end
 
 end
