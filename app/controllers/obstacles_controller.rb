57c9f8a2cd169f1c3580eb3d3aeef481ae6e6daf
Rosibel Cisneros <kelsocisneros@gmail.com>
2017-07-26 12:13:57 -0700

    username link to profile added panel to obstacles

--- app/controllers/obstacles_controller.rb
+++ app/controllers/obstacles_controller.rb
@@ -7,16 +7,17 @@ class ObstaclesController < ApplicationController
 
     def new
       @obstacle = Obstacle.new
+      
     end
 
     def create
       @obstacle = Obstacle.new(params.require(:obstacle).permit(:obstacle_content))
         @obstacle.person = current_peep 
-          if @obstacle.save
-            redirect_to obstacles_path
-          else
-            render :new
-          end
+            if @obstacle.save
+              redirect_to obstacles_path
+            else
+              render :new
+            end
     end
 
 end
