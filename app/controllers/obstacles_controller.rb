21868ffbb20418ab71a581ba776ca9aea431691f
Rosibel Cisneros <kelsocisneros@gmail.com>
2017-07-27 09:29:32 -0700

    profile image

--- app/controllers/obstacles_controller.rb
+++ app/controllers/obstacles_controller.rb
@@ -17,11 +17,11 @@ class ObstaclesController < ApplicationController
     def create
       @obstacle = Obstacle.new(params.require(:obstacle).permit(:obstacle_content))
         @obstacle.person = current_peep 
-            if @obstacle.save
-              redirect_to obstacles_path
-            else
-              render :new
-            end
+          if @obstacle.save
+            redirect_to obstacles_path
+          else
+            render :new
+          end
     end
 
 end
